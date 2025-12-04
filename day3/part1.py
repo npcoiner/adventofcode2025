90347298d25f566cb3aba4a49a24da400567bafc
npcoiner <nicholascoiner@gmail.com>
2025-12-03 20:35:50 -0800

    Finished part1 day3

--- day3/part1.py
+++ day3/part1.py
@@ -2,7 +2,6 @@ import sys
 
 
 def doNothing():
-    print("")
     return
 
 
@@ -10,15 +9,23 @@ def main(filename):
     with open(filename, 'r') as file:
         sum = 0
         for line in file:
-            print(line)
             maxl = int(line[0])
             maxr = int(line[1])
+            print(maxl, maxr, end="", sep="")
+            for i in range(1, len(line) - 1):
+                print(line[i], sep="", end="")
+                val = int(line[i])
+                if maxl < val and i < len(line) - 2:
+                    maxl = val
+                    maxr = int(line[i+1])
+                elif maxr < val:
+                    maxr = val
+            print("")
+            print(maxl, maxr, end="", sep="")
+            print("")
 
-            for i in line:
-                try:
-                    print(int(i))
-                except ValueError:
-                    doNothing()
+            sum += int(str(maxl) + str(maxr))
+        print(sum)
 
 
 if __name__ == "__main__":
